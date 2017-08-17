85c7d08e7a4a8291eb9550db8d026e5aeda34acc
hell0x <327466897@qq.com>
2017-08-17 14:32:31 +0800

    Python

---
+++ samples/model/model.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+' a test module '
+
+__author__ = 'Wei Xing'
+
+
+import sys
+
+
+def main():
+    args = sys.argv
+    if (len(args)) == 1:
+        print('Hello World')
+    elif len(args) == 2:
+        print('Hello %s' % args[1])
+    else:
+        print('Too many arguments')
+
+
+if __name__ == '__main__':
+    main()
